fa84ed5728170c8859d7b8e694bcff7a9de22335
Florian Gyger <gygy1992@hotmail.com>
2021-06-30 18:41:22 +0200

    feat: added honeypot field to protect from spam bots

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,12 @@ export const App = ({ config }: Props) => {
 
   const onFinish = async (values: Store) => {
     const { author, text } = values as NewEntry;
+
+    const isSpam = !!values.name;
+    if (isSpam) {
+      throw new Error('Possible spam bot detected. Form was not submitted.');
+    }
+
     const createdEntry = await createEntry({ author, text });
 
     if (!createdEntry) {
@@ -61,6 +67,14 @@ export const App = ({ config }: Props) => {
         {!submittedEntryId ? (
           <Card title={t('Leave an entry')} bodyStyle={{ paddingBottom: 0 }}>
             <Form layout="vertical" form={form} initialValues={{ author: '', text: '' }} onFinish={onFinish}>
+              <Form.Item
+                style={{ display: 'none' }}
+                name="name"
+                label={t('Please leave this field blank as it is used for spam protection.')}
+                rules={[{ required: false }]}
+              >
+                <Input />
+              </Form.Item>
               <Form.Item
                 name="author"
                 label={t('Author')}
