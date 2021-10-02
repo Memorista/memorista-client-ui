056c95fdd3284aee14940637048a0c84b1130047
Florian Gyger <gygy1992@hotmail.com>
2021-10-02 15:08:13 +0200

    feat: add list of open source licenses to bundle and link it in index.js (#19)

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,10 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const LicensePlugin = require('webpack-license-plugin');
+const { BannerPlugin } = require('webpack');
+const packageInformation = require('./package.json');
+
+const licenseFileUrl = `https://unpkg.com/${packageInformation.name}@${packageInformation.version}/dist/oss-licenses.json`;
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.tsx'),
@@ -25,6 +31,23 @@ module.exports = {
         test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
         use: ['file-loader'],
       },
+    ],
+  },
+  plugins: [
+    new LicensePlugin({
+      licenseOverrides: {
+        'identicon.js@2.3.3': 'BSD-2-Clause',
+      },
+    }),
+    new BannerPlugin({
+      banner: `For license information please see: ${licenseFileUrl}`,
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
     ],
   },
 };
