a9c80eada3ebea4d3450e052640400c5f6968b5b
Emin Khateeb <emin@emin.ch>
2021-11-25 19:54:52 +0100

    Added dotenv support

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
@@ -8,6 +9,7 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: { contentBase: path.resolve(__dirname, './dist') },
   plugins: [
+    new Dotenv(),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html'),
     }),
